1e530660ef27f720b418ace6dcdfa72a68c6ae9a
Albert Moreno <albertmorenovendrell@gmail.com>
2014-01-13 23:37:58 +0100

    Initial Commit

---
+++ RoutesGP/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RoutesGP
+//
+//  Created by albert moreno vendrell on 13/01/14.
+//  Copyright (c) 2014 RoutesGP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RoutesGP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoutesGP
+//
+//  Created by albert moreno vendrell on 13/01/14.
+//  Copyright (c) 2014 RoutesGP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
